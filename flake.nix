5cccaeabeb06798f1e7e7f62bbc7ac020b9c8270
polygonalbones <polybones@proton.me>
2026-01-13 12:06:16 -0500

    add devshell

--- flake.nix
+++ flake.nix
@@ -17,10 +17,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
-        lib = pkgs.lib;
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
+        zen-c = pkgs.stdenv.mkDerivation {
           pname = "zen-c";
           version = "unstable";
 
@@ -28,13 +25,20 @@
 
           env.PREFIX = placeholder "out";
 
-          meta = {
+          meta = with pkgs; {
             description = "Zen-C programming language compiler";
             homepage = "https://github.com/z-libs/Zen-C";
             license = lib.licenses.mit;
             platforms = lib.platforms.unix;
             mainProgram = "zc";
           };
+        };
+      in
+      {
+        packages.default = zen-c;
+
+        devShells.default = pkgs.mkShell {
+          packages = [ zen-c ];
         };
       }
     );
